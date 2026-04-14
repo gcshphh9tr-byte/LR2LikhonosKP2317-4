fd4d9cd909d773f2fd9e22b0a446e581d5ef680a
gcshphh9tr-byte <gcshphh9tr@privaterelay.appleid.com>
2026-04-14 21:20:10 +0000

    Пробное изменение

--- main.py
+++ main.py
@@ -1,281 +1,101 @@
-# # '''#Общая часть
-# # #1
-# #Контейнер расчета
-# from sympy import *
-# k, T, C, L = symbols('k T C L')
-# #1 способ
-# C_ost = 100000
-# Am_lst = []
-# C_ost_lst = []
-# for i in range (5):
-#   Am = (C - L )/ T
-#   C_ost -= Am.subs({C:100000, T:5, L:0})
-#   Am_lst.append(round(Am.subs({C:100000, T:5, L:0}),2))
-#   C_ost_lst.append(round(C_ost,2))
-# print('Am_lst:', Am_lst)
-# print('C_ost_lst:', C_ost_lst)
 
-# #2 
-# Aj=0
-# C_ost = 100000
-# Am_lst_2 = []
-# C_ost_lst_2 = []
-# for i in range (5):
-#   Am = k * 1/ T * (C - Aj)
-#   C_ost -= Am.subs({C:100000, T:5, k:2})
-#   Am_lst_2.append(round(Am.subs({C:100000, T:5, k:2}),2))
-#   Aj += Am
-#   C_ost_lst_2.append(round(C_ost,2))
-# print('Am_lst_2:', Am_lst_2)
-# print ('C_ost_lst_2:', C_ost_lst_2)
+# 3 ЛР Общая часть
+# 1 часть делала с Рощиной
 
-# #3
-# #Контейнер расчета
-# from sympy import *
-# k, T, C, L = symbols('k T C L')
+import os
+from sympy import *
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
-# #1 способ
-# C_ost_3 = 30000
-# Am_lst_3 = []
-# C_ost_lst_3 = []
-# for i in range (8):
-#    Am = (C - L )/ T
-#    C_ost_3 -= Am.subs({C:30000, T:8, L:0})
-#    Am_lst_3.append(round(Am.subs({C:30000, T:8, L:0}),2))
-#    C_ost_lst_3.append(round(C_ost_3,2))
-# print('Am_lst_3:', Am_lst_3)
-# print('C_ost_lst_3:', C_ost_lst_3)
+SESSION_SECRET = os.environ['SESSION_SECRET']
+print(SESSION_SECRET)
 
-# #2 способ
-# Aj = 0
-# C_ost_3 = 30000
-# Am_lst_2_3 = []
-# C_ost_lst_2_3 = []
-# for i in range (8):
-#    Am = k * 1/ T * (C - Aj)
-#    C_ost_3 -= Am.subs({C:30000, T:8, k:2})
-#    Am_lst_2_3.append(round(Am.subs({C:30000, T:8, k:2}),2))
-#    Aj += Am
-#    C_ost_lst_2_3.append(round(C_ost_3,2))
-# print  ('Am_lst_2_3:', Am_lst_2_3)
-# print ('C_ost_lst_2_3:', C_ost_lst_2_3)
+POLI = os.environ['POLI']
+print(POLI)
 
-# #Контейнер табличного вывода
-# import pandas as pd
-# Y = range (1,9)
-# table1 = list(zip( Y, Am_lst_3, C_ost_lst_3))
-# table2 = list(zip( Y, Am_lst_2_3, C_ost_lst_2_3))
-# tfame = pd.DataFrame(table1, columns = ['Y', 'Am_lst_3', 'C_ost_lst_3'])
-# tfame2 = pd.DataFrame(table2, columns = ['Y', 'Am_lst_2_3', 'C_ost_lst_2_3'])
-# print(tfame)
-# print(tfame2)
-
-# #Контейнер визуализации
-# import numpy as np
-# import matplotlib.pyplot as plt
-# plt.plot (tfame['Y'], tfame ['C_ost_lst_3'], label = 'Am')
-# plt.savefig('chart1.png')
-# plt.figure()
-# plt.plot (tfame2['Y'], tfame2 ['C_ost_lst_2_3'], label = 'Am_2')
-# plt.savefig('chart2.png')
-
-# vals = Am_lst_3
-# labels = [str(x) for x in range (1,9)]
-# explde = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
-# fig, ax = plt.subplots()
-# ax.pie (vals, labels = labels, autopct = '%1.1f%%', shadow = True, explode = explde, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':"k"}, rotatelabels = True)
-# ax.axis("equal")
-# plt.savefig('chart3.png')
-
-# vals = Am_lst_2_3
-# labels = [str(x) for x in range (1,9)]
-# explde = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
-# fig, ax = plt.subplots()
-# ax.pie (vals, labels = labels, autopct = '%1.1f%%', shadow = True, explode = explde, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':"k"}, rotatelabels = True)
-# ax.axis("equal")
-# plt.savefig('chart4.png')
-
-# #Гистограммы
-# table1 = list(zip(Y, Am_lst_3))
-# table2 =  list(zip(Y, Am_lst_2_3))
-# tfame = pd.DataFrame(table1, columns = ['Y', 'Am_lst_3'])
-# tfame2 = pd.DataFrame(table2, columns = ['Y', 'Am_lst_2_3'])
-
-# plt.figure()
-# plt.bar (tfame['Y'], tfame['Am_lst_3'])
-# plt.savefig('chart5.png')
-
-# plt.figure()
-# plt.bar (tfame2['Y'], tfame2['Am_lst_2_3'])
-# plt.savefig('chart6.png')
-# plt.figure()
-
-# #Индивидуальная часть
-# from sympy import *
-# k, T, C, L = symbols('k T C L')
-
-# #1 способ
-# C_ost_4 = 90000
-# Am_lst_4 = []
-# C_ost_lst_4 = []
-# for i in range (9):
-#    Am = (C - L )/ T
-#    C_ost_4 -= Am.subs({C:90000, T:9, L:0})
-#    Am_lst_4.append(round(Am.subs({C:90000, T:9, L:0}),2))
-#    C_ost_lst_4.append(round(C_ost_4,2))
-# print('Am_lst_4:', Am_lst_4)
-# print('C_ost_lst_4:', C_ost_lst_4)
-
-# #2 способ
-# Aj = 0
-# C_ost_4 = 90000
-# Am_lst_2_4 = []
-# C_ost_lst_2_4 = []
-# for i in range (9):
-#    Am = k * 1/ T * (C - Aj)
-#    C_ost_4 -= Am.subs({C:90000, T:9, k:2})
-#    Am_lst_2_4.append(round(Am.subs({C:90000, T:9, k:2}),2))
-#    Aj += Am
-#    C_ost_lst_2_4.append(round(C_ost_4,2))
-# print  ('Am_lst_2_4:', Am_lst_2_4)
-# print ('C_ost_lst_2_4:', C_ost_lst_2_4)
-
-# #Контейнер табличного вывода
-# import pandas as pd
-# Y = range (1,10)
-# table1 = list(zip( Y, Am_lst_4, C_ost_lst_4))
-# table2 = list(zip( Y, Am_lst_2_4, C_ost_lst_2_4))
-# tfame = pd.DataFrame(table1, columns = ['Y', 'Am_lst_4', 'C_ost_lst_4'])
-# tfame2 = pd.DataFrame(table2, columns = ['Y', 'Am_lst_2_4', 'C_ost_lst_2_4'])
-# print(tfame)
-# print(tfame2)
-
-# #Контейнер визуализации
-# import numpy as np
-# import matplotlib.pyplot as plt
-# plt.plot (tfame['Y'], tfame ['C_ost_lst_4'], label = 'Am')
-# plt.savefig('chart7.png')
-# plt.figure()
-# plt.plot (tfame2['Y'], tfame2 ['C_ost_lst_2_4'], label = 'Am_2')
-# plt.savefig('chart8.png')
-
-# vals = Am_lst_4
-# labels = [str(x) for x in range (1,10)]
-# explde = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
-# fig, ax = plt.subplots()
-# ax.pie (vals, labels = labels, autopct = '%1.1f%%', shadow = True, explode = explde, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':"k"}, rotatelabels = True)
-# ax.axis("equal")
-# plt.savefig('chart9.png')
-
-# vals = Am_lst_2_4
-# labels = [str(x) for x in range (1,10)]
-# explde = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
-# fig, ax = plt.subplots()
-# ax.pie (vals, labels = labels, autopct = '%1.1f%%', shadow = True, explode = explde, wedgeprops = {'lw':1, 'ls':'--', 'edgecolor':"k"}, rotatelabels = True)
-# ax.axis("equal")
-# plt.savefig('chart10.png')
-
-# #Гистограммы
-# table1 = list(zip(Y, Am_lst_4))
-# table2 =  list(zip(Y, Am_lst_2_4))
-# tfame = pd.DataFrame(table1, columns = ['Y', 'Am_lst_4'])
-# tfame2 = pd.DataFrame(table2, columns = ['Y', 'Am_lst_2_4'])
-
-# plt.figure()
-# plt.bar (tfame['Y'], tfame['Am_lst_4'])
-# plt.savefig('chart11.png')
-
-# plt.figure()
-# plt.bar (tfame2['Y'], tfame2['Am_lst_2_4'])
-# plt.savefig('chart12.png')'''
-
-
-
-#3 ЛР Общая часть
-#1 часть делала с Рощиной
-#print(Lihonos)
- 
-#SESSION_SECRET=os.environ['SESSION_SECRET']
-#print(SESSION_SECRET)
-
-#POLI=os.environ['POLI']
-#print(POLI)
-
-#2 часть делала с Мамедовой
+# 2 часть делала с Мамедовой
 # Индивидуальный вариант 2
-# from sympy import *
-# k, T, C, L = symbols ('k C T L')
-# #1 способ
-# C_ost=50000
-# Am_lst=[]
-# C_ost_lst=[]
-# for i in range(9):
-#   Am = (C-L)/T
-#   C_ost -= Am.subs({C: 50000, T:9, L:0})
-#   Am_lst.append (round (Am.subs({C: 50000, T:9, L:0}), 2))
-#   C_ost_lst.append(round(C_ost, 2))
-# print ('Am_lst:', Am_lst)
-# print ('Am_lst:', C_ost_lst)
-# #2 способ
-# Aj=0
-# C_ost=50000
-# Am_lst_2= []
-# C_ost_lst_2=[]
-# for i in range(9):
-#    Am = k * 1/T * (C-Aj)
-#    C_ost -= Am.subs({C: 50000, T:9, k:2})
-#    Am_lst_2.append (round (Am.subs({C: 50000, T:9, k:2}), 2))
-#    Aj += Am
-#    C_ost_lst_2.append(round(C_ost, 2))
-# print ('Am_lst_2:', Am_lst_2)
-# print ('C_ost_lst_2:', C_ost_lst_2)
+k, T, C, L = symbols('k C T L')
 
-# #Табличный вывод
-# import pandas  as pd
-# Y = range(1,10)
-# table1 = list(zip(Y, C_ost_lst, Am_lst))
-# table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
-# tfame = pd. DataFrame(table1, columns=['Y', 'C_ost_lst', 'Am_lst'])
-# tfame2 = pd. DataFrame(table2, columns=['Y', 'C_ost_lst_2', 'Am_lst_2'])
-# print(tfame)
-# print(tfame2)
-# #Визуализация
-# import numpy as np
-# import matplotlib.pyplot as plt
-# plt.plot(tfame['Y'], tfame ['C_ost_lst'], label = 'Am')
-# plt.savefig ('chart13.png')
-# plt.figure()
-# plt.plot(tfame2['Y'], tfame2 ['C_ost_lst_2'], label = 'Am_2')
-# plt.savefig ('chart14.png')
-# vals = Am_lst
-# labels = [str(x) for x in range(1,10)]
-# explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
-# fig, ax = plt.subplots()
-# ax.pie (vals, labels=labels, autopct='%1.1f%%', shadow=True,
-#          explode=explode, wedgeprops={'lw':1, 'ls':'--', 'edgecolor': "k"},
-#          rotatelabels=True)
-# ax.axis("equal")
-# plt.savefig ('chart15.png')
-# vals = Am_lst_2
-# labels = [str(x) for x in range(1,10)]
-# explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
-# fig, ax = plt.subplots()
-# ax.pie(vals, labels=labels, autopct='%1.1f%%', shadow=True,
-#         explode=explode, wedgeprops={'lw':1, 'ls':'--', 'edgecolor': "k"},
-#         rotatelabels=True) # что это? это настройки отображения диаграммы, которые включают в себя ширину линии (lw), стиль линии (ls) и цвет линии (edgecolor), а также поворот меток (rotatelabels) Оценка: 5/5 всё верно
-# ax.axis("equal")
-# plt.savefig ('chart16.png')
-# plt.figure()
-# table1 = list(zip(Y, Am_lst))
-# table2 = list(zip(Y, Am_lst_2))
-# tfame = pd.DataFrame(table1, columns = ['Y', 'Am_lst'])
-# tfame2 = pd.DataFrame(table2, columns = ['Y', 'Am_lst_2'])
-# plt.bar(tfame['Y'], tfame['Am_lst'])
-# plt.savefig ('chart17.jpeg')
-# plt.figure()
-# plt.bar(tfame['Y'], tfame2['Am_lst_2'])
-# plt.savefig ('chart18.png')
-# plt.figure()
+# 1 способ
+C_ost = 70000
+Am_lst = []
+C_ost_lst = []
+for i in range(8):
+    Am = (C - L) / T
+    C_ost -= Am.subs({C: 70000, T: 8, L: 0})
+    Am_lst.append(round(Am.subs({C: 70000, T: 8, L: 0}), 2))
+    C_ost_lst.append(round(C_ost, 2)) # что это? Эта строка добавляет новое значение в конец списка C_ost_lst, предварительно округляя его до 2 знаков после запятой.
+print('Am_lst:', Am_lst)
+print('C_ost_lst:', C_ost_lst)
+
+# 2 способ 
+Aj = 0
+C_ost = 70000
+Am_lst_2 = []
+C_ost_lst_2 = []
+for i in range(8):
+    Am = 2 * 1/8 * (70000 - Aj)  # k=2, T=6
+    Am = round(Am, 2)
+    C_ost -= Am
+    C_ost = round(C_ost, 2)
+    Am_lst_2.append(Am)
+    C_ost_lst_2.append(C_ost)
+    Aj += Am
+print('Am_lst_2:', Am_lst_2)
+print('C_ost_lst_2:', C_ost_lst_2)
+
+# Табличный вывод
+Y = range(1, 9)  
+table1 = list(zip(Y, C_ost_lst, Am_lst))
+table2 = list(zip(Y, C_ost_lst_2, Am_lst_2))
+tfame = pd.DataFrame(table1, columns=['Y', 'C_ost_lst', 'Am_lst'])
+tfame2 = pd.DataFrame(table2, columns=['Y', 'C_ost_lst_2', 'Am_lst_2'])
+print(tfame)
+print(tfame2)
+
+# Визуализация
+plt.plot(tfame['Y'], tfame['C_ost_lst'], label='Am')
+plt.savefig('chart13.png')
+plt.figure()
+plt.plot(tfame2['Y'], tfame2['C_ost_lst_2'], label='Am_2')
+plt.savefig('chart14.png')
+
+# Первая круговая диаграмма
+vals = Am_lst
+labels = [str(x) for x in range(1, 9)]  
+explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)
+fig, ax = plt.subplots()
+ax.pie(vals, labels=labels, autopct='%1.1f%%', shadow=True,
+       explode=explode, wedgeprops={'lw': 1, 'ls': '--', 'edgecolor': "k"},
+       rotatelabels=True)
+ax.axis("equal")
+plt.savefig('chart15.png')
+
+# Вторая круговая диаграмма 
+vals = Am_lst_2
+labels = [str(x) for x in range(1, 9)]  
+explode = (0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1)  
+fig, ax = plt.subplots()
+ax.pie(vals, labels=labels, autopct='%1.1f%%', shadow=True,
+       explode=explode, wedgeprops={'lw': 1, 'ls': '--', 'edgecolor': "k"},
+       rotatelabels=True)
+ax.axis("equal")
+plt.savefig('chart16.png')
+plt.figure()
+
+# Столбчатые диаграммы
+table1 = list(zip(Y, Am_lst))
+table2 = list(zip(Y, Am_lst_2)) #что это? Эта строка объединяет три списка в один список кортежей 
+tfame = pd.DataFrame(table1, columns=['Y', 'Am_lst'])
+tfame2 = pd.DataFrame(table2, columns=['Y', 'Am_lst_2'])
+plt.bar(tfame['Y'], tfame['Am_lst'])
+plt.savefig('chart17.jpeg')
+plt.figure()
+plt.bar(tfame['Y'], tfame2['Am_lst_2'])
+plt.savefig('chart18.png')
+plt.figure() # что это? это настройки отображения диаграммы, которые включают в себя ширину линии (lw), стиль линии (ls) и цвет линии (edgecolor), а также поворот меток (rotatelabels)
 # Выполнено корректно, ставлю 5/5
 
 # Задание 3
@@ -284,4 +104,172 @@
 # Задание 4
 # Выполняла с Мамедовой
 # строка 264
+
+
+# import pandas as pd
+# import matplotlib.pyplot as plt
+
+# # Тестовые cookies. В реальном приложении сюда можно подставить свои cookies.
+# cookies = [
+#     {
+#         "name": "session_id", "secure": False, "httponly": False,
+#         "samesite": None, "domain": ".example.com",
+#         "max_age": None, "host_prefix": False,
+#     },
+#     {
+#         "name": "user_token", "secure": True, "httponly": True,
+#         "samesite": "Strict", "domain": "app.example.com",
+#         "max_age": 60 * 60 * 24 * 365, "host_prefix": False,
+#     },
+#     {
+#         "name": "tracking_id", "secure": False, "httponly": False,
+#         "samesite": "None", "domain": ".example.com",
+#         "max_age": None, "host_prefix": False,
+#     },
+#     {
+#         "name": "__Host-csrf", "secure": True, "httponly": True,
+#         "samesite": "Strict", "domain": "app.example.com",
+#         "max_age": 3600, "host_prefix": True,
+#     },
+# ]
+
+
+# def add_threat(threats, cookie, category, threat, level, recommendation):
+#     # Добавляем найденную угрозу в общий список.
+#     threats.append({
+#         "Cookie": cookie["name"],
+#         "Категория": category,
+#         "Угроза": threat,
+#         "Уровень": level,
+#     })
+
+
+# def analyze_cookie(cookie):
+#     # Проверяем одну cookie на разные виды угроз.
+#     threats = []
+
+#     # Угроза перехвата: cookie передаётся не только по HTTPS.
+#     if not cookie["secure"]:
+#         add_threat(threats, cookie, "Перехват",
+#                    "Cookie может быть перехвачена при передаче по HTTP",
+#                    "Критический", "Добавить флаг Secure")
+
+#     # Угроза перехвата/CSRF: cookie отправляется при межсайтовых запросах.
+#     if cookie["samesite"] is None or cookie["samesite"] == "None":
+#         add_threat(threats, cookie, "Перехват",
+#                    "Cookie может передаваться при межсайтовых запросах",
+#                    "Высокий", "Использовать SameSite=Lax или Strict")
+
+#     # Угроза доступа: JavaScript может прочитать cookie при XSS-атаке.
+#     if not cookie["httponly"]:
+#         add_threat(threats, cookie, "Доступ",
+#                    "Cookie доступна через JavaScript при XSS-атаке",
+#                    "Критический", "Добавить флаг HttpOnly")
+
+#     # Угроза доступа: cookie видна всем поддоменам.
+#     if cookie["domain"].startswith("."):
+#         add_threat(threats, cookie, "Доступ",
+#                    "Cookie доступна всем поддоменам",
+#                    "Средний", "Указать точный домен без точки в начале")
+
+#     # Угроза изменения: cookie не защищена префиксом __Host-.
+#     if not cookie["host_prefix"]:
+#         add_threat(threats, cookie, "Изменение",
+#                    "Cookie не использует защитный префикс __Host-",
+#                    "Высокий", "Использовать имя cookie с префиксом __Host-")
+
+#     # Угроза изменения: cookie можно перехватить и подменить.
+#     if not cookie["secure"] and not cookie["httponly"]:
+#         add_threat(threats, cookie, "Изменение",
+#                    "Cookie можно подменить через Man-in-the-Middle",
+#                    "Критический", "Добавить Secure и HttpOnly")
+
+#     # Угроза изменения: cookie живёт неопределённо долго или слишком долго.
+#     month = 60 * 60 * 24 * 30
+#     if cookie["max_age"] is None:
+#         add_threat(threats, cookie, "Изменение",
+#                    "Cookie не имеет заданного срока жизни",
+#                    "Средний", "Установить max_age или expires")
+#     elif cookie["max_age"] > month:
+#         add_threat(threats, cookie, "Изменение",
+#                    "Cookie хранится слишком долго",
+#                    "Средний", "Сократить срок хранения cookie")
+
+#     return threats
+
+
+# def analyze_all_cookies(cookies_list):
+#     # Проверяем все cookies и создаём таблицу pandas.
+#     all_threats = []
+#     for cookie in cookies_list:
+#         all_threats.extend(analyze_cookie(cookie))
+
+#     df = pd.DataFrame(all_threats)
+#     levels = ["Критический", "Высокий", "Средний", "Низкий"]
+#     df["Уровень"] = pd.Categorical(df["Уровень"], categories=levels, ordered=True)
+#     df = df.sort_values(["Уровень", "Категория"]).reset_index(drop=True)
+#     df.index += 1
+#     df.index.name = "№"
+#     return df
+
+
+# def show_summary(df):
+#     # Считаем количество угроз перехвата, доступа и изменения.
+#     counts = df["Категория"].value_counts()
+#     print("\nКоличество угроз по категориям:")
+#     print(f"Перехват:  {counts.get('Перехват', 0)}")
+#     print(f"Доступ:    {counts.get('Угроза доступа', 0)}")
+#     print(f"Изменение: {counts.get('Изменение', 0)}")
+#     return counts
+
+
+# def build_charts(counts):
+#     # Строим круговую диаграмму в отдельном PNG-файле.
+#     colors = ["#ff6b6b", "#ffd166", "#4dabf7"]
+#     plt.figure(figsize=(7, 6))
+#     plt.pie(counts.values, labels=counts.index, autopct="%1.1f%%", colors=colors)
+#     plt.title("Круговая диаграмма угроз")
+#     plt.tight_layout()
+#     plt.savefig("cookie_threats_pie.png", dpi=150)
+#     plt.close()
+
+#     # Строим гистограмму во втором отдельном PNG-файле.
+#     plt.figure(figsize=(7, 6))
+#     plt.bar(counts.index, counts.values, color=colors)
+#     plt.title("Гистограмма угроз")
+#     plt.xlabel("Угроза доступа")
+#     plt.ylabel("Количество")
+#     plt.tight_layout()
+#     plt.savefig("cookie_threats_histogram.png", dpi=150)
+#     plt.close()
+
+
+# def main():
+#     # Запускаем анализ, выводим таблицу, сохраняем CSV и графики.
+#     df = analyze_all_cookies(cookies)
+#     print("\nТаблица найденных угроз:")
+#     print(df.to_string())
+
+#     df.to_csv("cookie_threats_report.csv", encoding="utf-8-sig")
+#     counts = show_summary(df)
+#     build_charts(counts)
+
+#     print("\nФайлы сохранены:")
+#     print("cookie_threats_report.csv — таблица угроз")
+#     print("cookie_threats_pie.png — круговая диаграмма")
+#     print("cookie_threats_histogram.png — гистограмма")
+
+
+# if __name__ == "__main__":
+#     main()
+# '''
+# Описание функций:
+
+# 1. add_threat() — добавляет найденную угрозу в список угроз.
+# 2. analyze_cookie() — проверяет одну cookie на перехват, доступ и изменение.
+# 3. analyze_all_cookies() — проверяет все cookies и создаёт pandas DataFrame.
+# 4. show_summary() — выводит количество угроз по категориям.
+# 5. build_charts() — строит круговую диаграмму и гистограмму в отдельных PNG-файлах.
+# 6. main() — запускает программу, сохраняет CSV-отчёт и PNG-графики.
+# '''
 
